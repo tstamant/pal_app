861d6f52e1ad6e9e9bdb43f7bef131e1afc2e799
Tony St. Amant <tony@gtamail.com>
2024-09-15 13:15:50 -0300

    Add Nav bar & page titles

--- app.rb
+++ app.rb
@@ -1,14 +1,17 @@
 require 'sinatra'
 
 get '/' do
+  @title ='Home'
   erb :index
 end
 
 get '/about' do
+  @title ='About'
   erb :about
 end
 
 get '/palindrome' do
+  @title ='Palindrome Detector'
   erb :palindrome
 end
  
--- test/site_pages_test.rb
+++ test/site_pages_test.rb
@@ -8,22 +8,35 @@ class PalindromeAppTest < Minitest::Test
     Sinatra::Application
   end
  
+  def setup
+    @base_title = "Learn Enough Ruby Sample App"
+  end
+   
   def test_index
     get '/'
     assert last_response.ok?
     assert doc(last_response).at_css('h1')
+    title_content = doc(last_response).at_css('title').content
+    assert_equal "#{@base_title} | Home", title_content
+    assert doc(last_response).at_css('nav')
   end
  
   def test_about
     get '/about'
     assert last_response.ok?
     assert doc(last_response).at_css('h1')
+    title_content = doc(last_response).at_css('title').content
+    assert_equal "#{@base_title} | About", title_content
+    assert doc(last_response).at_css('nav')
   end
 
   def test_palindrome
     get '/palindrome'
     assert last_response.ok?
     assert doc(last_response).at_css('h1')
+    title_content = doc(last_response).at_css('title').content
+    assert_equal "#{@base_title} | Palindrome Detector", title_content
+    assert doc(last_response).at_css('nav')
   end
 
 end
